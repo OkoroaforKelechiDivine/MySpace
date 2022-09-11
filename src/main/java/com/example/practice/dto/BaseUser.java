def2f8e306e66be796091c830fea77dfb31bc0ac
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-09-11 02:13:21 +0100

    Done with dto

--- src/main/java/com/example/practice/dto/BaseUser.java
+++ src/main/java/com/example/practice/dto/BaseUser.java
@@ -1,4 +1,18 @@
 package com.example.practice.dto;
 
-public class BaseUser {
+import com.example.practice.dto.User.AppUserType;
+import lombok.Data;
+
+@Data
+public abstract class BaseUser {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private String email;
+
+    private AppUserType userType;
 }
